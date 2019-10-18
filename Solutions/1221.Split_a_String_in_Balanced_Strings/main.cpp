dc7c9aa232375b8f1342b1429cb516fd22237439
awr417h <abdullahwaqar8@gmail.com>
2019-10-18 20:21:23 +0500

    1221.Split_a_String_in_Balanced_Strings

---
+++ Solutions/1221.Split_a_String_in_Balanced_Strings/main.cpp
@@ -0,0 +1,52 @@
+/*
+Balanced strings are those who have equal quantity of 'L' and 'R' characters.
+
+Given a balanced string s split it in the maximum amount of balanced strings.
+
+Return the maximum amount of splitted balanced strings.
+
+
+
+Example 1:
+
+Input: s = "RLRRLLRLRL"
+Output: 4
+Explanation: s can be split into "RL", "RRLL", "RL", "RL", each substring contains same number of 'L' and 'R'.
+
+Example 2:
+
+Input: s = "RLLLLRRRLR"
+Output: 3
+Explanation: s can be split into "RL", "LLLRRR", "LR", each substring contains same number of 'L' and 'R'.
+
+Example 3:
+
+Input: s = "LLLLRRRR"
+Output: 1
+Explanation: s can be split into "LLLLRRRR".
+
+
+
+Constraints:
+
+    1 <= s.length <= 1000
+    s[i] = 'L' or 'R'
+
+
+*/
+
+#include <iostream>
+
+class Solution {
+public:
+    int balancedStringSplit(std::string s) {
+        int temp, answer = 0;
+        for (char c : s) {
+            temp += (c == 'L') ? 1 : -1;
+            if (temp == 0) {
+                answer++;
+            }
+        }
+        return answer;
+    }
+};
