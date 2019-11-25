07b0b6f2f6083b8f448fb569b34331ec1ca8770b
awr417h <abdullahwaqar8@gmail.com>
2019-11-25 13:41:23 +0500

    205.Isomorphic_Strings

---
+++ Solutions/205.Isomorphic_Strings/main.py
@@ -0,0 +1,6 @@
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        return len(set(zip(s, t))) == len(set(s)) == len(set(t))
+
+if __name__ == "__main__":
+    Solution().isIsomorphic('egg', 'add')
