43d80f5fdf440f4629b266aa66f199bad720e32a
awr417h <abdullahwaqar8@gmail.com>
2020-06-20 22:30:54 +0500

    Move 0s

--- 30-day-leetcoding-challenge(2020-04-05)/Move-Zeros/main.cpp
+++ 30-day-leetcoding-challenge(2020-04-05)/Move-Zeros/main.cpp
@@ -1,16 +1,39 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
 class Solution {
 public:
-    void moveZeroes(vector<int>& nums) {
-        int n = nums.size();
-        int next = 0;
-        for (int x: nums) {
-            if (x != 0) {
-                nums[next] = x;
-                next++; 
+    void moveZeroes(std::vector<int>& nums) {
+        int zero_count = 0;
+        int size = nums.size();
+
+        for (int n : nums) {
+            if (n != 0) {
+                nums[zero_count] = n;
+                ++zero_count;
             }
         }
-        for (int i = 0; i < n; i++) {
+
+        for (int i = zero_count; i < size; ++i)
             nums[i] = 0;
-        }
+
+        std::for_each(nums.begin(), nums.end(), [&](int el) {
+            std::cout << el << '-' << zero_count << std::endl;
+        });
     }
 };
+
+int main(int argc, char const *argv[]) {
+    // std::vector<int> test{0,1,0,3,12};
+    // Solution().moveZeroes(test);
+
+    std::vector<int> test1{0, 0, 1};
+    Solution().moveZeroes(test1);
+
+    std::cout << std::endl << std::endl << std::endl;
+
+    std::vector<int> test2{0,1,0,3,12};
+    Solution().moveZeroes(test2);
+    return 0;
+}
