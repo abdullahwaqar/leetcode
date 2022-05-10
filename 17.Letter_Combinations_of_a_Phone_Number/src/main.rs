f574a7ce7902321afce273b4f15f265712fcb5bc
aw <awr417h@pm.me>
2022-05-10 20:46:17 +0500

    17. Letter Combinations of a Phone Number

--- 17.Letter_Combinations_of_a_Phone_Number/src/main.rs
+++ 17.Letter_Combinations_of_a_Phone_Number/src/main.rs
@@ -2,19 +2,41 @@ struct Solution {}
 
 impl Solution {
     pub fn letter_combinations(digits: String) -> Vec<String> {
-        let mut digits = digits.chars();
+        let mut result = vec![];
+
+        if digits.is_empty() {
+            return result;
+        }
+
         let letters = vec![
             "", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz",
         ];
 
-        for digit in digits {
-            let _char = letters[digit.to_digit(10).unwrap() as usize]
-                .chars()
-                .collect::<>();
-            println!("{:?}", _char);
+        for digit in digits.chars() {
+            let chars = letters[digit.to_digit(10).unwrap() as usize].chars();
+
+            if result.is_empty() {
+                result = chars.into_iter().map(|c| c.to_string()).collect();
+            } else {
+                // fucking slow
+                result = result
+                    .iter()
+                    .flat_map(|elem| {
+                        chars
+                            .clone()
+                            .into_iter()
+                            .map(|c| {
+                                let mut res = elem.clone();
+                                res.push(c.clone());
+                                res
+                            })
+                            .collect::<Vec<_>>()
+                    })
+                    .collect()
+            }
         }
 
-        return vec![];
+        return result;
     }
 }
 
