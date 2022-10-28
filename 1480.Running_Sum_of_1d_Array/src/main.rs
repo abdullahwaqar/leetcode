077f509592ba8e575c1a51c47b51a29582b92ec2
aw <awr417h@pm.me>
2022-10-29 03:31:03 +0500

    1480. Running Sum of 1d Array

---
+++ 1480.Running_Sum_of_1d_Array/src/main.rs
@@ -0,0 +1,16 @@
+struct Solution {}
+
+impl Solution {
+    pub fn running_sum(mut nums: Vec<i32>) -> Vec<i32> {
+
+        for idx in 1..nums.len() {
+            nums[idx] += nums[idx -1];
+        }
+
+        return nums;
+    }
+}
+
+fn main() {
+    println!("{:?}", Solution::running_sum(vec![1, 2, 3, 4]));
+}
